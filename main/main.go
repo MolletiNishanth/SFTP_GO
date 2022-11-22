b57ef91a00a8f1b682c371829eaee0c9f299f39d
NishanthMolleti <molleti.nishanth@rakuten.com>
2022-11-22 18:14:53 +0530

    sftp to gcs working successfully

--- main/main.go
+++ main/main.go
@@ -7,8 +7,10 @@ import (
 	 "fmt"
 	 "os"
 	// "io/ioutil"
-    // "log"
+     "log"
 	"strings"
+	"context"
+	"cloud.google.com/go/storage"
 	"io"
 	"golang.org/x/crypto/ssh"
     "golang.org/x/crypto/ssh/agent"
@@ -16,67 +18,88 @@ import (
 	"github.com/gin-gonic/gin"
 	"errors"
 	"path/filepath"
+	"mime/multipart"
+	"time"
 )
-type book struct {
+const (
+	projectID  = "gcp-infra-test-321021"  // FILL IN WITH YOURS
+	bucketName = "nishanth_bucket" // FILL IN WITH YOURS
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+var uploader *ClientUploader
+func init() {
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", "/Users/molleti.nishanth/Downloads/gcp-infra-test-321021-ba44e3282946.json") // FILL IN WITH YOUR FILE PATH
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	uploader = &ClientUploader{
+		cl:         client,
+		bucketName: bucketName,
+		projectID:  projectID,
+		uploadPath: "test-files/",
+	}
+
+}
+type files struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
 }
 
-var books = []book{
-	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
-	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
-	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
-}
-// type file struct{
-// 	ID          string  `json:"id"`
-// 	FILENAME    string  `json:"filename"`
-// 	RESPONSE    int     `json:"response"`
-// 	DATA        string	`json:"data"`
-// }
-// var files = []file{
-// 	{ID: "1",FILENAME: "file", RESPONSE: 1, DATA: "Hello1"} ,
-// 	{ID: "2",FILENAME: "file2", RESPONSE: 2, DATA: "Hello2"} ,
-// }
-//  func getFiles(c *gin.Context){
-//  	c.IndentedJSON(http.StatusOK, files)
-//  }
-//  func fileData(c *gin.Context){
-// 	id:=c.Param("id")
-// 	fmt.Println(id)
-// 	file,err:=getFileData(id)
-// 	 if err != nil{
-// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
-// 		return 
-// 	 }
-// 	 c.IndentedJSON(http.StatusOK,file)
-//  }
-//  func getFileData(id string) (*file ,  error){
-// 	for i, b := range files{
-// 		if b.ID==id {
-// 			return &files[i], nil 
-// 		}
-// 	}
-// 	return nil , errors.New("File not found")
-// }
-func bookById(c *gin.Context) {
-	id := c.Param("id")
-	book, err := getBookById(id)
+func filesById(c *gin.Context) {
+	filess := c.Param("id")
+	_, err := getfilesById(filess)
 
-	if err != nil {
-		c.IndentedJSON(http.StatusOK, gin.H{"message": "Book not found."})
+	if filess=="sample.txt" {
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "File found."})
+		// f, err := c.FormFile("file_input")
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{
+		// 		"error": err.Error(),
+		// 	})
+		// 	return
+		// }
+
+		blobFile, err := /*f.Open()*/ os.Open("sample.txt")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		err = uploader.UploadFile(blobFile, "sample.txt")			// "./sample.txt" is put in place of f.Filename
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"message": "success",
+		})
 		return
 	}
-	c.IndentedJSON(http.StatusOK, book)
+	if err!=nil {
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "file not found !! "})
+		return 
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message ": "File not found "})
 }
 
-func getBookById(id string) ( *book, error) {
-	// for i, b := range books {
-	// 	if b.ID == id {
-	// 		return &books[i], nil
-	// 	}
-	// }
+func getfilesById(id string) ( *files, error) {
+	
 	if _, err := os.Stat("sample.txt"); err == nil {
 		if "sample.txt"==id {                           //how to typecast id into file type 
 		fmt.Println("File exists !!! ");
@@ -138,13 +161,14 @@ func getBookById(id string) ( *book, error) {
 		uploadFile(*sc, "./sample.txt", "./remote.txt")
 		downloadFile(*sc, "./remote.txt", "./local.txt")
 		
+		
 	 }
 	 } else {
 		
 		fmt.Printf("File does not exist");
 	 }
 
-	return nil, errors.New("book not found")
+	return nil, errors.New("files not found")
 }
 func downloadFile(sc sftp.Client, remoteFile, localFile string) (err error) {
 
@@ -209,63 +233,26 @@ func uploadFile(sc sftp.Client, localFile, remoteFile string) (err error) {
 	
 	return
 }
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
 func main()  {
-	user := "Nishanth, Molleti"
-    pass/*, _*/ := "Drowssap@271199"
-
-    // Parse Host and Port
-    host := "localhost"
-    // Default SFTP port
-    port := 22
-
-//    hostKey := getHostKey(host)
-
-    fmt.Fprintf(os.Stdout, "Connecting to %s ...\n", host)
-
-    var auths []ssh.AuthMethod
-
-    // Try to use $SSH_AUTH_SOCK which contains the path of the unix file socket that the sshd agent uses 
-    // for communication with other processes.
-    if aconn, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
-        auths = append(auths, ssh.PublicKeysCallback(agent.NewClient(aconn).Signers))
-    }
-
-    // Use password authentication if provided
-    if pass != "" {
-        auths = append(auths, ssh.Password(pass))
-    }
-    
-    // Initialize client configuration
-    config := ssh.ClientConfig{
-        User: user,
-        Auth: auths,
-        // Uncomment to ignore host key check
-        HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-      //  HostKeyCallback: ssh.FixedHostKey(hostKey),
-    }
-
-    addr := fmt.Sprintf("%s:%d", host, port)
-
-    // Connect to server
-    conn, err := ssh.Dial("tcp", addr, &config)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to connecto to [%s]: %v\n", addr, err)
-        os.Exit(1)
-    }
-
-    defer conn.Close()
-
-    // Create new SFTP client
-    sc, err := sftp.NewClient(conn)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Unable to start SFTP subsystem: %v\n", err)
-        os.Exit(1)
-    }
-    defer sc.Close()
 	router := gin.Default()
-	// router.GET("/files",getFiles)
-	// router.GET("/files/:id ",fileData)
-	 router.GET("/books/:id",bookById)
+	 router.GET("/pullfile/:id",filesById)
 	 router.Run("localhost:8080")
 
 }
